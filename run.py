929a0629c944a8fa155d879576dec58f8abcdee7
Lillies <liov.mendonca@outlook.com>
2020-02-23 13:41:55 +0000

    Add ability to write and read from a list and display to user

--- run.py
+++ run.py
@@ -1,11 +1,28 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
+messages = []
+
+def add_message(username, message):
+    messages.append(f"{username}: {message}")
+
+def get_all_messages():
+    return "<br>".join(messages)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/<username>')
+def user(username):
+    return f"<h1>Welcome, {username}</h1> {get_all_messages()}" 
+
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    add_message(username, message)
+    
+    return redirect('/' + username)
 
 if __name__ == '__main__':
     app.run(host = os.environ.get('IP', '0.0.0.0'),
